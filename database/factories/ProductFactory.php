1951a97ef8aa9fb7b8d6688d81b592ae75bd9722
NkwiCyril <akinimbomnkwi@gmail.com>
2025-02-27 21:25:36 +0100

    feat: product and purchase factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\PurchaseGoal;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'description' => $this->faker->sentence,
+            'unit_price' => $this->faker->randomFloat(2, 5, 500),
+            'bulk_price' => $this->faker->randomFloat(2, 50, 5000),
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'image' => 'https://placehold.co/300x300/',
+            'purchase_goal_id' => PurchaseGoal::factory(),
+        ];
+    }
+}
